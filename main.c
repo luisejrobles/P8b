cf4bac9c521293d3e2b9e404142b15bebf003d59
luisejrobles <luisejimenezrobles@gmail.com>
2017-04-21 17:59:37 -0700

    archivos iniciales

---
+++ main.c
@@ -0,0 +1,30 @@
+/*
+ * P8b.c
+ *
+ * Created: 4/21/2017 5:42:46 PM
+ * Author : LuisEduardo
+ */ 
+
+#include <avr/io.h>
+#define BUFFER_SIZE 64
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
+void putchar(char data)
+{
+	if(!IS_BUFFER_FULL(bufferTX))
+	{
+		
+	}
+}
+
+ISR(USA)
+
