42c8f2d6b0107d04e65da3d36669c2a935a28e01
luisejrobles <luisejimenezrobles@gmail.com>
2017-04-27 22:35:33 -0700

    validaciones rdy

--- main.c
+++ main.c
@@ -1,30 +1,93 @@
 /*
- * P8b.c
- *
- * Created: 4/21/2017 5:42:46 PM
- * Author : LuisEduardo
+	Universidad Autonoma de Baja California
+	Microprocesadores y Microcontroladores
+		Jimenez Robles Luis Eduardo
+				01208396
  */ 
-
 #include <avr/io.h>
 #define BUFFER_SIZE 64
 
+typedef struct {
+	char buffer[BUFFER_SIZE];       //espacio reservado 
+	volatile unsigned char in_idx;  //indice entrada (Head) 
+	volatile unsigned char out_idx; //indice salida (tail)
+	volatile unsigned int in_pos;	//posicion de in
+	volatile unsigned int out_pos;	//posicion de out
+} ring_buffer_t;
+
+void BUFFER_INIT(struct ring_buffer_t *buffer);
 
 
 int main(void)
 {
-    /* Replace with your application code */
+    ring_buffer_t transmision;
+	ring_buffer_t recepcion;
+	
+	BUFFER_INIT(&transmision);
+	BUFFER_INIT(&recepcion);
+	
     while (1) 
     {
     }
 }
 
-void putchar(char data)
+void BUFFER_INIT(struct ring_buffer_t *buffer)
 {
-	if(!IS_BUFFER_FULL(bufferTX))
+	buffer->in_idx = 0;
+	buffer->out_idx = 0;
+	buffer->in_pos = 0;
+	buffer->out_pos = 0;
+}
+
+void putchar(char data, struct ring_buffer_t)
+{
+	
+}
+void puts(char *cad)
+{
+	while( !(IS_BUFFER_FULL()) && *cad)
 	{
-		
+				
 	}
 }
 
-ISR(USA)
-
+char UART0_getchar(char c)
+{
+	while( IS_BUFFER_FULL() )
+	{
+		
+	}
+	
+}
+int IS_BUFFER_FULL(int in_pos, int out_pos)
+{
+	if(in == (out-1))
+	{
+		return 1;
+	}
+	return 0;
+}
+int IS_BUFFER_EMTPY(int in_pos,int out_pos)
+{
+	if(in_pos == out_pos)
+	{
+		return 1;
+	}
+	return 0;
+}
+uint8_t UART0_available(int in_pos,int out_pos)
+{
+	if( (in_pos != (out_pos -1)) && (in_pos == out_pos) )
+	{
+		return 1;
+	}
+	return 0;
+}
+ISR(SIG_USART0_RECV)
+{
+	
+}
+ISR(SIG_USART0_DATA)
+{
+	
+}
