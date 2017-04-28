a3361013a08c5b5f0f913a024404c047a4f08e06
luisejrobles <luisejimenezrobles@gmail.com>
2017-04-28 14:50:44 -0700

    NO MUESTRA

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
  */ 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
 #define BUFFER_SIZE 64
 #define BUFFER_EMPY(buffer) (buffer.in_idx == buffer.out_idx)
 #define BUFFER_FULL(buffer) (buffer.in_idx == (buffer.out_idx-1))
@@ -21,7 +22,10 @@ typedef struct {
 
 char UART0_getchar(void);
 uint8_t UART0_available(void);
+unsigned int atoi(char *str);
+void itoa(char *str, uint16_t number, uint8_t base);
 void BUFFER_INIT(ring_buffer_t buffer);
+void UART0_gets(char *str);
 void UART0_puts(char *cad);
 void UART0_putchar(char data);
 void UART0_Init(uint16_t mode);
@@ -29,14 +33,62 @@ void UART0_Init(uint16_t mode);
 ring_buffer_t transmision;
 ring_buffer_t recepcion;
 
+
 int main(void)
 {	
+	char cad[20];
+	uint16_t num;
+	UART0_Init(0);
 	BUFFER_INIT(transmision);
 	BUFFER_INIT(recepcion);
 	
     while (1) 
     {
+		UART0_puts("\n\rUniversidad Autonoma de Baja California\n\r Practica 8a");
+		UART0_puts("\n\n\rIntroduce un número:\n\r");
+		UART0_gets(cad);
+		num = atoi(cad);
+		itoa(cad,num,10);
+		UART0_puts("\n\rDec:");
+		UART0_puts(cad);
+		itoa(cad,num,2);
+		UART0_puts("\n\rBin:");
+		UART0_puts(cad);
     }
+	
+}
+void itoa(char *str, uint16_t number, uint8_t base)
+{
+	unsigned int cociente, residuo,count = 0, i=0, j;
+	char c;
+	cociente = number;
+	do{
+		residuo = cociente%base;
+		cociente = cociente/base;
+		if(residuo > 9)	//si es mayor a 9, agregar el respectivo para imprimir letra.
+		{
+			c = residuo + 55;
+			}else{
+			c = residuo + '0'; //agregar el respectivo para crear el caracter de numero
+		}
+		*str++ = c;
+		count++;
+	}while( cociente != 0 );
+	*str= '\0';
+	str -=count;
+	j = count -1;
+	//==============invertir cadena==================
+	while(i < j)
+	{
+		if( *(str+i) != *(str+j))
+		{
+			c = *(str+i);
+			*(str+i) = *(str+j);
+			*(str+j) = c;
+		}
+		i++;
+		j--;
+	}
 }
 void UART0_Init(uint16_t mode)
 {
@@ -59,11 +111,34 @@ void BUFFER_INIT(ring_buffer_t buffer)
 	buffer.in_idx = 0;
 	buffer.out_idx = 0;
 }
+void UART0_gets(char *str)
+{
+	unsigned char c;
+	unsigned int i=0;
+	do{
+		c = UART0_getchar();
+		if( (i<=18)&&(c!=8)&&(c!=13) )	//validacion menor al fin del arreglo, backspace y enter
+		{
+			UART0_putchar(c);
+			*str++ = c;
+			i++;
+		}
+		if( (c==8) && (i>0) )		//validacion backspace
+		{
+			UART0_putchar('\b');
+			UART0_putchar(' ');
+			UART0_putchar(8);
+			*str--='\0';
+			i--;
+		}
+	}while(c != 13);
+	*str = '\0';
+}
 void UART0_puts(char *cad)
 {	
 	while(*cad)
 	{
-		*cad++;
+		UART0_putchar(*cad++);
 	}
 }
 void UART0_putchar(char data)
@@ -72,40 +147,60 @@ void UART0_putchar(char data)
 	{
 		transmision.buffer[transmision.in_idx] = data;	//guardado char en el arreglo
 		transmision.in_idx = MOD(transmision.in_idx++);	//incrementando pocision en el arreglo
-		TX_IN_EN;	//tx interrupt enable
+		//TX_IN_EN;	//tx interrupt enable
+		UCSR0B |= (1<<UDRIE0);
 	}
 }
 char UART0_getchar(void)
 {
 	char c;
-	while( !(BUFFER_EMPY(recepcion)))
-	{
-		RX_IN_EN;	//rx interrupt enable
-		c = recepcion.buffer[recepcion.out_idx];	//guardando dato de pos del arreglo
-		recepcion.out_idx = MOD(recepcion.out_idx++);	//incrementando out
-		return c;
-	}
+	while( !(UCSR0A&(1<<RXC0)));
+	
+	UCSR0B |= (1<< RXCIE0);
+	//RX_IN_EN;	//rx interrupt enable
+	c = recepcion.buffer[recepcion.out_idx];	//guardando dato de la posicion del arreglo
+	recepcion.out_idx = MOD(recepcion.out_idx++);	//incrementando out
+	
+	return c;
 }
 uint8_t UART0_available(void)
 {
-	if( !(BUFFER_FULL(transmision)))
+	if( !(BUFFER_FULL(recepcion)))
 	{
 		return 1;
 	}
 	return 0;
 }
+unsigned int atoi(char *str)
+{
+	unsigned int num = 0, exp = 1, val, count = 0;
+	//contando digitos en la cadena============
+	while(*str)
+	{
+		str++;
+		count++;
+	}
+	str--;	//no tomando en cuenta '\0'
+	while(count != 0)
+	{
+		val = *str--;	//tomando el valor
+		val = val - '0';	//obteniendo valor crudo
+		num = num + (val * exp);	//almacenando valor crudo*exp en num
+		exp = exp*10;
+		count--;
+	}
+	return num;
+}
 ISR(USART0_RX_vect)	//Rx interrupt
 {
 	recepcion.buffer[recepcion.in_idx] = UDR0;
-	recepcion.out_idx = MOD(recepcion.in_idx++);
-	//UCSR0B =	
+	recepcion.in_idx = MOD(recepcion.in_idx++);
+	UCSR0B &= (127<<TXB80);
 }
 ISR(USART0_UDRE_vect)	//tx interrupt
 {
 	UDR0 = transmision.buffer[transmision.out_idx];
 	transmision.out_idx = MOD(transmision.out_idx++);
-	while(!(UCSR0A &(1<<UDRE0)))
-	{
-		UCSR0B &= 216<<TXB80;
-	}
+	while(!(UCSR0A&(1<<UDRE0)));
+	UCSR0B &= (223<<TXB80);
 }
